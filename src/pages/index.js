d18e94e49db6fdfaf0b25c16930a6ebe64d61281
Charlezton <carl.elgestad@gmail.com>
2020-10-31 13:00:04 +0100

    * Import entry data to index page * Import images instead of using static folder manually

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,12 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Timeline from '../components/timeline';
-
-const entries = [
-  {
-    title: 'Ruby... my first love',
-    subtitle: 'Senior highschool',
-    description: 'awdawdawd',
-    date: '2010',
-    icon: '/images/ruby.png',
-  },
-  {
-    title: 'BA in computer science',
-    subtitle: 'Stockholm University',
-    description: 'awdawdawd',
-    date: '2011-2015',
-  },
-  {
-    title: 'IT-Support',
-    subtitle: 'Viktor Rydberg Djursholm',
-    description: 'awdawdawd',
-    date: '2013-2014',
-  },
-  {
-    title: 'Developer',
-    subtitle: 'SWECO',
-    description: 'awdawdawd',
-    date: '2015',
-  },
-  {
-    title: 'Senior Developer',
-    subtitle: 'Digitalent',
-    description: 'awdawdawd',
-    date: '2019',
-  },
-  {
-    title: 'Tech Lead',
-    subtitle: 'Mindler',
-    description: 'awdawdawd',
-    date: '2021-present',
-  },
-];
+import entries from '../data/timelineEntries';
 
 export default function Home() {
   return (
     <Layout>
+      {/* <Hero/> */}
       <Timeline entries={entries} />
     </Layout>
   );
