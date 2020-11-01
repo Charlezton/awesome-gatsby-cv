12b26e336417b90f79c619ff365fed4778d7cfa3
Charlezton <carl.elgestad@gmail.com>
2020-11-01 22:09:49 +0100

    Implement hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Timeline from '../components/timeline';
-import entries from '../data/timelineEntries';
+import Hero from '../components/hero';
+import timelineEntries from '../data/timelineEntries';
+import heroContent from '../data/heroContent';
 
 export default function Home() {
   return (
     <Layout>
-      {/* <Hero/> */}
-      <Timeline entries={entries} />
+      <Hero
+        title={heroContent.title}
+        description={heroContent.description}
+        buttonText={heroContent.buttonText}
+      />
+      <Timeline entries={timelineEntries} />
     </Layout>
   );
 }
