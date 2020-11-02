03a7d63cd38b7b00298d0a768051b8e9d16816be
Charlezton <carl.elgestad@gmail.com>
2020-11-02 23:06:08 +0100

    Add new images
    Use img instead of div tag for hero
    
    Load hero image via import

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,39 +1,42 @@
 import React from 'react';
+import tech from '../images/tech.jpg';
 
 export default function Hero(props) {
   return (
-    <div style={styles.image}>
+    <div style={styles.hero}>
       <div style={styles.textContentWrapper}>
         <h1 style={styles.title}>{props.title}</h1>
         <p style={styles.description}>{props.description}</p>
         {/* <button>{props.buttonText}</button> */}
       </div>
+      <img style={styles.image} src={tech} alt="keyboard" />
     </div>
   );
 }
 
 const styles = {
-  image: {
-    height: '100vh',
-    backgroundImage:
-      'url(https://images.unsplash.com/photo-1500417148159-68083bd7333a)',
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
+  hero: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   textContentWrapper: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
     fontFamily: 'NineteenNinetySeven',
     color: '#E5E9F0',
-    height: '100%',
+    position: 'absolute',
   },
   title: {
-    fontSize: '10vh',
+    fontSize: '5em',
   },
   description: {
-    fontSize: '3vh',
+    fontSize: '2em',
+  },
+  image: {
+    height: '100vh',
+    width: '100%',
   },
 };
