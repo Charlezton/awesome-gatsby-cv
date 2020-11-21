070db35cae4a64351621dabfe0718f68a633a2bf
Charlezton <carl.elgestad@gmail.com>
2020-11-21 10:52:47 +0100

    Do not resize screen on horizontal animation

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 
 const Background = styled.div`
   background-color: #2e3440;
+  overflow-x: hidden;
+  overflow-y: auto;
 `;
 
 export default function Layout({ children }) {
